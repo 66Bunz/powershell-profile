1073831cb348b48c3410e1fa286daa2da2e44d17
bunz <66bunz@gmail.com>
2025-02-24 18:29:29 +0100

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -21,6 +21,7 @@ else { 'notepad' }
 
 $global:canConnectToGitHub = $null -ne (ping github.com -n 1 -w 1000 | Select-String "Reply from")
 
+Install-Module -Name PSReadLine -Force
 if (-not (Get-Module -ListAvailable -Name Terminal-Icons)) {
     Install-Module -Name Terminal-Icons -Scope CurrentUser -Force -SkipPublisherCheck
 }
@@ -57,12 +58,15 @@ $PSReadLineOptions = @{
         Error     = '#FF6347'  # Tomato (keeping it close to red for visibility)
         Selection = '#e829f7'  # Black
     }
+    PredictionViewStyle           = 'ListView'
     BellStyle                     = 'None'
 }
 
 if ($PSVersionTable.PSVersion.Major -ge 7) {
     Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-    Set-PSReadLineOption -PredictionVewStyle ListView
+}
+else {
+    Set-PSReadLineOption -PredictionSource History
 }
 Set-PSReadLineOption @PSReadLineOptions
 
@@ -421,7 +425,7 @@ function which($name) {
 #region Python
 function py-env {
     param (
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [ValidateSet("create", "activate", "deactivate", "delete")]
         [string]$Command
     )
@@ -436,7 +440,8 @@ function py-env {
             if (Test-Path ".\.venv\Scripts\Activate") {
                 Write-Host "Activating Python virtual environment..." -ForegroundColor Green
                 & .\.venv\Scripts\Activate
-            } else {
+            }
+            else {
                 Write-Host "Error: Virtual environment not found. Run 'py-env create' first." -ForegroundColor Red
             }
         }
@@ -449,7 +454,8 @@ function py-env {
                 Write-Host "Deleting Python virtual environment..." -ForegroundColor Yellow
                 Remove-Item -Recurse -Force .\.venv
                 Write-Host "Virtual environment deleted successfully!" -ForegroundColor Green
-            } else {
+            }
+            else {
                 Write-Host "No virtual environment found to delete." -ForegroundColor Red
             }
         }
